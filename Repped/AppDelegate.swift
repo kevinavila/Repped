e4d2f07ac8cb58d6f30149a4eb66b47bcc603939
Kevin Avila <kevin.avila@utexas.edu>
2017-02-16 16:39:49 -0600

    Implemented Apple Music membership/playback check methods (not tested yet).

--- Repped/AppDelegate.swift
+++ Repped/AppDelegate.swift
@@ -9,6 +9,8 @@
 import UIKit
 import Firebase
 import FBSDKLoginKit
+import StoreKit
+import MediaPlayer
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -42,10 +44,78 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
         FBSDKAppEvents.activateApp()
+        checkIfUserHasAppleMusic()
+        requestAppleMusicPermission()
+        
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    
+    // Check if user has Apple Music membership
+    func checkIfUserHasAppleMusic() {
+        let serviceController = SKCloudServiceController()
+        serviceController.requestCapabilities(completionHandler: { (capability:SKCloudServiceCapability, err:Error?) in
+            if (err != nil) {
+                print("An error occured when trying to validate Apple Music membership.")
+            }
+            switch capability {
+                
+            case SKCloudServiceCapability.musicCatalogPlayback:
+                print("The user has an Apple Music subscription and can playback music!")
+                
+            case SKCloudServiceCapability.addToCloudMusicLibrary:
+                print("The user has an Apple Music subscription, can playback music AND can add to the Cloud Music Library")
+                
+            default:
+                print("The user doesn't have an Apple Music subscription available. Now would be a good time to prompt them to buy one?")
+                
+            }
+        })
+        
+    }
+    
+    // Request Apple Music Permission
+    func requestAppleMusicPermission() {
+        switch SKCloudServiceController.authorizationStatus() {
+            
+        case .authorized:
+            print("The user's already authorized - we don't need to do anything more here, so we'll exit early.")
+            return
+            
+        case .denied:
+            print("The user has selected 'Don't Allow' in the past - so we're going to show them a different dialog to push them through to their Settings page and change their mind, and exit the function early.")
+            // Show an alert to guide users into the Settings
+            return
+            
+        case .notDetermined:
+            print("The user hasn't decided yet - so we'll break out of the switch and ask them.")
+            break
+            
+        case .restricted:
+            print("User may be restricted; for example, if the device is in Education mode, it limits external Apple Music usage. This is similar behaviour to Denied.")
+            return
+        }
+        
+        SKCloudServiceController.requestAuthorization { (status:SKCloudServiceAuthorizationStatus) in
+            switch status {
+                
+            case .authorized:
+                print("All good - the user tapped 'OK', so you're clear to move forward and start playing.")
+                
+            case .denied:
+                print("The user tapped 'Don't allow'.")
+                
+            case .notDetermined:
+                print("The user hasn't decided or it's not clear whether they've confirmed or denied.")
+                
+            default: break
+                
+            }
+            
+        }
+        
     }
 
 

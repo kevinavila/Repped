ce8e998e378d9dcdf3a804e76816f07ab8c39877
Kevin Avila <kevin.avila@utexas.edu>
2017-02-25 20:16:55 -0600

    You can now go to the Room view and click a button that plays back the song Lonely by Nav. SAID SHE SCARED TO POP A PILL (YEAHHHHHHH)

--- Repped/RoomController.swift
+++ Repped/RoomController.swift
@@ -7,12 +7,50 @@
 //
 
 import UIKit
+import MediaPlayer
+import StoreKit
 
 class RoomController: UIViewController {
+    
+    let systemMusicPlayer = MPMusicPlayerController.systemMusicPlayer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    @IBAction func playNav(_ sender: Any) {
+        systemMusicPlayer.setQueueWithStoreIDs(["1207859520"])
+        systemMusicPlayer.play()
+    }
+    
+    
+    // Fetch the user's storefront ID
+    func appleMusicFetchStorefrontRegion() {
+        let serviceController = SKCloudServiceController()
+        
+        serviceController.requestStorefrontIdentifier(completionHandler: { (storefrontId:String?, err:Error?) in
+            
+            guard err == nil else {
+                
+                print("An error occured. Handle it here.")
+                return
+                
+            }
+            
+            guard let storefrontId = storefrontId, storefrontId.characters.count >= 6 else {
+                
+                print("Handle the error - the callback didn't contain a valid storefrontID.")
+                return
+                
+            }
+            
+            let start = storefrontId.startIndex
+            let end = storefrontId.index(storefrontId.startIndex, offsetBy: 5)
+            let indexRange = start..<end
+            let trimmedId = storefrontId[indexRange]
+            
+            print("Success! The user's storefront ID is: \(trimmedId)")
+        })
+    }
     
 }

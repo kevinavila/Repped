b3239fbb787dce798bfa9c6ad72a050c50d7f8e6
Kevin Avila <kevin.avila@utexas.edu>
2017-02-25 19:22:21 -0600

    Created Room storyboard and controller.

--- Repped/RoomController.swift
+++ Repped/RoomController.swift
@@ -1,14 +1,14 @@
 //
-//  HomeController.swift
+//  RoomController.swift
 //  Repped
 //
-//  Created by Kevin Avila on 2/17/17.
+//  Created by Kevin Avila on 2/25/17.
 //  Copyright © 2017 Audiophiles. All rights reserved.
 //
 
 import UIKit
 
-class HomeController: UIViewController {
+class RoomController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()

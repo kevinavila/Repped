b64e36ab5bb039aeb18eb80cf2579c6a71c77788
Kevin Avila <kevin.avila@utexas.edu>
2017-02-26 22:29:02 -0600

    Created Room model. Recreated home screen as a table view. Starting room/channel work.

--- Repped/HomeController.swift
+++ Repped/HomeController.swift
@@ -7,12 +7,26 @@
 //
 
 import UIKit
+import Firebase
 
-class HomeController: UIViewController {
+class HomeController: UITableViewController {
+    
+    var senderName: String?
+    private var rooms: [Room] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
     
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
 }
---
+++ Repped/Room.swift
@@ -0,0 +1,18 @@
+//
+//  Room.swift
+//  Repped
+//
+//  Created by Kevin Avila on 2/26/17.
+//  Copyright © 2017 Audiophiles. All rights reserved.
+//
+
+internal class Room {
+    
+    internal let id:String
+    internal let name:String
+    
+    init(id:String, name:String) {
+        self.id = id
+        self.name = name
+    }
+}

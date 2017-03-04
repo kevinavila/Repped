64d70b6b5db9a74cfba2bfab5187743c19fdd4f3
Kevin Avila <kevin.avila@utexas.edu>
2017-03-03 20:33:29 -0600

    Added AddFriends controller and storyboard. Segue is currently corrupted though.

---
+++ Repped/AddFriendsController.swift
@@ -0,0 +1,17 @@
+//
+//  AddFriendsController.swift
+//  Repped
+//
+//  Created by Kevin Avila on 3/3/17.
+//  Copyright © 2017 Audiophiles. All rights reserved.
+//
+
+import UIKit
+
+class AddFriendsController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}

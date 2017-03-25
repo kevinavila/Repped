332abd2c0732eca0653b20784f40d09f02eea6ee
Kevin Avila <kevin.avila@utexas.edu>
2017-03-25 16:57:27 -0500

    Re-did Profile screen UI. Added rep, LIVE indicator, and add friend option. LIVE indicator and add friend option are not function-complete yet.

--- Repped/ProfileController.swift
+++ Repped/ProfileController.swift
@@ -12,18 +12,26 @@ import Firebase
 class ProfileController: UIViewController{
     
     @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var roomLabel: UILabel!
     @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var repLabel: UILabel!
+    @IBOutlet weak var liveSymbol: UIView!
+    @IBOutlet weak var liveLabel: UILabel!
+    @IBOutlet weak var addFriendButton: UIButton!
+    
     var global:Global = Global.sharedGlobal
     var user:User? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.profileImage.image = self.user?.profilePicture
+        self.profileImage.layer.cornerRadius = self.profileImage.frame.size.width / 2
+        self.profileImage.clipsToBounds = true
+        self.profileImage.layer.borderWidth = 3.0
         
         self.nameLabel.text = self.user?.name
-        self.roomLabel.text = self.global.room?.name
-        self.profileImage.image = self.user?.profilePicture
+        self.repLabel.text = String(describing: (self.user?.rep)!)
+        
     }
     
     
